a04ded39ff167e7243cc642ec463e7c911f57add
Atakan Arslan <70545078+atakanarslan3268@users.noreply.github.com>
2025-07-19 22:46:21 +0300

    Add search bar and startup cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,25 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default function Home() {
+export default async function Home({ searchParams }: {
+    searchParams: Promise<{ query?: string}>
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    { 
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'Atakan'},
+    _id: 1,
+    description: 'This is a description.',
+    image: "https://i.postimg.cc/yNGxNnkD/fb-Ati.png",
+    category: "Football",
+    title: "Buying Match Tickets",
+  },
+];
+
   return (
     <>
       <section className="pink_container">
@@ -9,8 +28,27 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
         </p>
+
+        <SearchForm query={query}></SearchForm>
+
       </section>
 
+      <section className="section_container">
+        <p className="text-30-semibold">
+          { query ? ` Search results for "${query}" ` : 'All Startups' }
+
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?.id} post={post} />
+            ))
+          ): (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
 
 
 
