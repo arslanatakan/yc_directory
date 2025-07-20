235edc412c0ac638000a2fa37c0271512ea087db
Atakan Arslan <70545078+atakanarslan3268@users.noreply.github.com>
2025-07-20 23:28:38 +0300

    Queries.ts icerisini farkli sekilde duzenlemek zorunda kaldim. Aksi takdirde page.tsx uzerinde bir hata meydana geliyordu.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+
 
 export default async function Home({ searchParams }: {
     searchParams: Promise<{ query?: string}>
 }) {
   const query = (await searchParams).query;
-
   const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
@@ -31,11 +31,11 @@ export default async function Home({ searchParams }: {
         </p>
 
         <ul className="mt-7 card_grid">
-          { posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
-          ): (
+          ) : (
             <p className="no-results">No startups found</p>
           )}
         </ul>
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,15 +1,36 @@
 import { defineQuery } from "next-sanity";
 
+// export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc){
+//     _id,
+//     title,
+//     slug,
+//     _createdAt,
+//     author -> {
+//       _id, name, image, bio
+//     }, 
+//     views,
+//     description,
+//     category,
+//     image
+// }`)
+
+//sanity/lib/queries.ts
 export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc){
+  _id,
+  _type,
+  _createdAt,
+  _updatedAt,
+  _rev,
+  title,
+  slug,
+  author->{
     _id,
-    title,
-    slug,
-    _createdAt,
-    author -> {
-      _id, name, image, bio
-    }, 
-    views,
-    description,
-    category,
-    image
-}`)
+    name,
+    image,
+    bio
+  },
+  views,
+  description,
+  category,
+  image
+}`);
