3c8bfa1be53da39b3099f08d2b0ea3917bfda37e
Atakan Arslan <70545078+atakanarslan3268@users.noreply.github.com>
2025-08-09 23:24:55 +0300

    Add editor picks, last edits etc.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -94,3 +94,29 @@ export const AUTHOR_BY_ID_QUERY =
     category,
     image
 }`);
+
+export const PLAYLIST_BY_SLUG_QUERY = defineQuery(`
+*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author->{
+      _id,
+      name,
+      image,
+      bio,
+      slug
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }
+}
+`);
